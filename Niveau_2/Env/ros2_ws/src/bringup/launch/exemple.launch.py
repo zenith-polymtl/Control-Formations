357c439eadcee3fea7004e8ccecb550c0d91dafd
Astro-Coco <colinc131@gmail.com>
2025-10-20 13:59:44 -0400

    fixed zenmav threshold

--- Niveau_2/Env/ros2_ws/src/bringup/launch/exemple.launch.py
+++ Niveau_2/Env/ros2_ws/src/bringup/launch/exemple.launch.py
@@ -2,28 +2,26 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
-    return LaunchDescription(
-        [
-            Node(
-                package="mission",
-                executable="exemple",  # mission/colin_drone_node.py : main()
-                name="exemple",
-                output="screen",
-                parameters=[{"look_ahead": 2.0, "takeoff_alt": 10.0}],
-            ),
-            Node(
-                package="mission_control",
-                executable="balloon",  # mission_control/ballon_pub.py : main()
-                name="ballon",
-                output="screen",
-            ),
-            Node(
-                package="mission_control",
-                executable="monitor",  # mission_control/monitor.py : main()
-                name="monitor",
-                output="screen",
-            ),
-        ]
-    )
+    return LaunchDescription([
+        Node(
+            package='mission',
+            executable='exemple',   # mission/colin_drone_node.py : main()
+            name='exemple',
+            output='screen',
+            parameters=[{'look_ahead' : 2.0,
+                         'takeoff_alt' : 10.0}]
+        ),
+        Node(
+            package='mission_control',
+            executable='balloon',         # mission_control/ballon_pub.py : main()
+            name='ballon',
+            output='screen',
+        ),
+        Node(
+            package='mission_control',
+            executable='monitor',            # mission_control/monitor.py : main()
+            name='monitor',
+            output='screen',
+        ),
+    ])
